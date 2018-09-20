9c9bc7f19740e3297db4f10e6cb643c588e13bb0
Stiven <stivencastillo.90@gmail.com>
2018-09-19 19:56:17 -0500

    Contador

--- src/Contador.js
+++ src/Contador.js
@@ -1,38 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 class Contador extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        contador:0
-      }
-    }
-    
-    aumentar(event) {
-      this.setState({contador:this.state.contador+1})
-      console.log(event.target.value);
-    }
-    decrementar(event) { 
-      let contador = this.state.contador
-      if(contador>0) {
-        this.setState({contador:contador-1})
-      }
-      console.log(event.target.value); 
-    }
-
-   
-    
-
-    render() {
-      return (
-        <div>
-          <h3>{this.state.contador}</h3>
-          <input type="button" onClick={this.aumentar.bind(this)}    value="+" />
-          <input type="button" onClick={this.decrementar.bind(this)} value="-" />
-        </div>
-      )
-    }
-    
+  render() {
+    return (
+      <div>
+        <h3>{this.props.value}</h3>
+        <input type="button" onClick={this.props.onIncrement} value="+" />
+        {/* <input type="button" onClick={this.decrementar.bind(this)} value="-" /> */}
+      </div>
+    );
   }
+}
 
-  export default Contador;
+export default Contador;
